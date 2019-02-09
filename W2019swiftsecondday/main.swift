ea16efd3ada343d205334cbb08c8aabd17a7e535
MacStudent <macstudent@126-iMac06.local>
2019-02-08 19:36:09 -0500

    Array and arrayslioce in swift

--- W2019swiftsecondday/main.swift
+++ W2019swiftsecondday/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-print("Hello, World!")
+/*print("Hello, World!")
 /*
 if(true == false)
 {
@@ -97,4 +97,80 @@ else{
 //closed range   e.g. 1..10
 //PartialRangeFrom  e.g.
 
+*/
+print("-----------------ARRAY------------------")
+
+var a = [10,20,30,40,50]
+print(a)
+var x = a
+print(x)
+
+
+for i in a
+{
+    print(i)
+}
+
+
+print("length: ", a.count)
+
+print("first element: ",a[0])
+print("last element: ",a[4])
+//print("element: ",a[5])
+
+
+var b = [Int]()
+//b[0] = 100
+
+
+b.append(100)
+b.append(200)
+b.append(300)
+print("B Array: ",b[0],b[1],b[2])
+
+b+=[111,222,333,444]
+print(b);
+
+
+print("Values of C array")
+print()
+let c = a + b;
+
+for i in c
+{
+    print(i)
+}
+
+print("---values of c[1..3] array in a loop---")
+
+var x1 = c[1...3]
+//x1[0]=2020
+x1[1]=2020
+
+for i in x1
+{
+    print(i)
+}
+
+print("c of 1 array: ",c[1])
+print();
+
+var x2 = c[2...4]
+
+for i in x2
+{
+    print("x2 array: ",i)
+}
+
+
+//var y = c[3...21] array index is out of range
+//print(y[2] index out of bounds
+
+
+print("---- Declaration using ARRAY type ----")
+
+
+var z=Array<Int>()
+z.append(500)
+print(z)
 
